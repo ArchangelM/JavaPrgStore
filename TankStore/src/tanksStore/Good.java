25dd3e5b21cff4cc4825d41110db1c54c4ebf0c3
Mihail Osipov <osarch@hotbox.ru>
2016-03-07 01:43:33 +0200

    First stores classes definitions

---
+++ TankStore/src/tanksStore/Good.java
@@ -0,0 +1,50 @@
+package tanksStore;
+
+/**
+ * Created by Osipov on 07.03.2016.
+ */
+public class Good {
+    protected int index;
+    private String name;
+    private String articul;
+
+    private double price;
+    private int quantity;
+
+    private double weight;
+
+
+    public Good(String name, String articul, int index, double price, int quantity, double weight) {
+        this.name = name;
+        this.articul = articul;
+        this.index = index;
+        this.price = price;
+        this.weight = weight;
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getIndex() {
+        return index;
+
+    }
+
+    public String getArticul() {
+        return articul;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
---
+++ TankStore/src/tanksStore/Purchase.java
@@ -0,0 +1,29 @@
+package tanksStore;
+
+
+/**
+ * Created by Osipov on 07.03.2016.
+ */
+public class Purchase {
+    private Good good;
+    private int quantity;
+    private double sum;
+
+   public Purchase(Good good, int quantity) {
+       this.good = good;
+       this.quantity = quantity;
+       sum = quantity * good.getPrice();
+   }
+
+    public Good getGood() {
+        return good;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+}
