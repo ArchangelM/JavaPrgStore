a7517d5c5e001c8e736792b0bc35decbf1563377
Mihail Osipov <osarch@hotbox.ru>
2016-06-12 22:04:15 +0300

    Jtab+

--- TankStore/src/tanksStore/LauncherStore.java
+++ TankStore/src/tanksStore/LauncherStore.java
@@ -2,9 +2,18 @@ package tanksStore;
 
 import tanksStore.gui.StoreInterface;
 
+import java.util.ArrayList;
+import java.util.List;
+import tanksStore.utils.Init;
+
+import static tanksStore.utils.Init.goodsInit;
+
 public class LauncherStore {
     public static void main(String[] args) {
         TankStore tankStore = new TankStore();
+        List<Good> goods = new ArrayList<>();
+
+        goodsInit(goods);
 
         StoreInterface panel = new StoreInterface(tankStore);
         panel.viewTable();
--- TankStore/src/tanksStore/utils/Init.java
+++ TankStore/src/tanksStore/utils/Init.java
@@ -1,8 +1,6 @@
 package tanksStore.utils;
 
-import tanksStore.Client;
-import tanksStore.Good;
-import tanksStore.SalesInvoice;
+import tanksStore.*;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -10,6 +8,29 @@ import java.util.List;
 import java.util.Random;
 
 public class Init {
+
+    public static void goodsInit(List<Good> goods) {
+        Good cur = new Tank("T34", "2345601", 1, 20000, 2, 50000);
+
+        goods.add(cur);
+        cur = new Tank("Tiger", "2345602", 2, 25000, 1, 55000);
+
+        goods.add(cur);
+        cur = new Armor("Passive", "2345701", 3, 2000, 5, 3000);
+        goods.add(cur);
+        cur = new Armor("Dynamic", "2345702", 4, 2000, 10, 4500);
+
+        goods.add(cur);
+        cur = new Ammo("Cummulative", "2345801", 5, 1000, 100, 50);
+        goods.add(cur);
+        cur = new Ammo("Piercing", "2345802", 6, 500, 80, 30);
+
+        goods.add(cur);
+        cur = new Engine("Maibah 1000 hp", "2345901", 7, 7000, 4, 50);
+        goods.add(cur);
+        cur = new Engine("B-2-34 500 hp", "2345902", 8, 3000, 3, 30);
+
+    }
 
     public static void transactionGenerator(List <SalesInvoice> si) {
         Random random = new Random(107894579);
